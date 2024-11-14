ad551bd4f387a2f75f1ac694b49b66f9b2273ffb
somedina0897 <somedina0897play@gmail.com>
2024-11-14 14:33:30 -0500

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Eliminar todas las variables de sesión
-session_destroy(); // Destruir la sesión actual
-
-// Redirigir al formulario de inicio de sesión
-header("Location: login.php");
-exit();
-?>
